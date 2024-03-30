c298afe77a198f07a0148b04b631b21fa43b4c7a
Ashutosh Kumar <132209941+AshutoshStark@users.noreply.github.com>
2024-03-31 04:51:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import './App.css';
 import Landing from './pages/Landing';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/Login';
-import SignUpPage from './pages/SignUpPage';
 import Home from './pages/Home';
 import { useStateContext } from './GlobalContext/ContextProvider';
 import User from './components/User/User';
